6779abf0d6e9f99b8b56928c6b0c979a7d1eb657
PawelZielinski <pawel.zielinski@bitapps.fi>
2026-01-22 12:07:27 +0100

    ADD: UIText class for managing strings both from resource and dynamically

---
+++ composeApp/src/commonMain/kotlin/org/example/pantrywisecmp/core/domain/UiText.kt
@@ -0,0 +1,21 @@
+package org.example.pantrywisecmp.core.domain
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
+
+sealed class UiText {
+    data class DynamicString(val value: String) : UiText()
+    data class StringResourceId(
+        val id: StringResource,
+        val args: Array<Any> = arrayOf()
+    ): UiText()
+
+    @Composable
+    fun asString(): String {
+        return when(this) {
+            is DynamicString -> value
+            is StringResourceId -> stringResource(id, args)
+        }
+    }
+}
