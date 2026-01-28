6605822386fa579b4f3ee2986ae42d5aefc81534
PawelZielinski <pawel.zielinski@bitapps.fi>
2026-01-28 18:12:27 +0100

    ADD: Producing recentProducts data in SuggestionsRepository

--- composeApp/src/commonMain/kotlin/org/example/pantrywisecmp/product/domain/repository/SuggestionsRepository.kt
+++ composeApp/src/commonMain/kotlin/org/example/pantrywisecmp/product/domain/repository/SuggestionsRepository.kt
@@ -1,8 +1,9 @@
 package org.example.pantrywisecmp.product.domain.repository
 
+import kotlinx.coroutines.flow.Flow
 import org.example.pantrywisecmp.product.domain.ProductSuggestion
 
 interface SuggestionsRepository {
     fun getSuggestions(query: String): List<ProductSuggestion>
-    fun getRecentProductsSuggestions(): List<ProductSuggestion>
+    fun getRecentProductsSuggestions():  Flow<List<ProductSuggestion>>
 }
