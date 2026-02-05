1dba2e9ec0013cadf28dd278e0e24a4391082402
PawelZielinski <pawel.zielinski@bitapps.fi>
2026-02-05 11:10:12 +0100

    ADD: @ConstructedBy annotation for PantryWiseDatabase

--- composeApp/src/commonMain/kotlin/org/example/pantrywisecmp/product/data/database/PantryWiseDatabase.kt
+++ composeApp/src/commonMain/kotlin/org/example/pantrywisecmp/product/data/database/PantryWiseDatabase.kt
@@ -1,11 +1,13 @@
 package org.example.pantrywisecmp.product.data.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [ProductEntity::class], version = 1, exportSchema = false)
 @TypeConverters(DatabaseConverters::class)
+@ConstructedBy(DatabaseConstructor::class)
 abstract class PantryWiseDatabase: RoomDatabase() {
 
     abstract fun productDao(): ProductDao
